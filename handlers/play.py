57b3c93460020b847e1775327f93a1cd68fa4fdc
looserakki <70113298+looserakki@users.noreply.github.com>
2021-10-08 10:16:52 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -8,7 +8,7 @@ from helpers.admins import get_administrators
 from os import path
 import requests
 import aiohttp
-import youtube_dl
+import yt_dlp
 from youtube_search import YoutubeSearch
 from pyrogram import filters, emoji
 from pyrogram.types import InputMediaPhoto
